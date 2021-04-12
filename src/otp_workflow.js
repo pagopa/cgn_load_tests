6369e4826c9f8c157c4f0123e56aae3943db36ab
Michele Ferracin <miferrac@microsoft.com>
2021-04-12 16:58:12 +0200

    load tests

---
+++ src/otp_workflow.js
@@ -0,0 +1,135 @@
+//docker run -i --rm -v $(pwd)/src:/src  -e FUNC_KEY=${FUNC_KEY} -e FUNC_MERCHANT_KEY=${FUNC_MERCHANT_KEY} -e BASE_URL=${BASE_URL} -e BASE_MERCHANT_URL=${BASE_MERCHANT_URL} loadimpact/k6 run /src/otp_workflow.js
+
+import http from 'k6/http';
+import { sleep } from 'k6';
+import { check } from 'k6';
+
+export let options = {
+    vus: 1,
+    stages: [
+        { duration: '30s', target: 1 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+    ],
+    thresholds: {
+        http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+        'http_req_duration{pagoPaMethod:OtpGeneration}': ['p(95)<1000'], // threshold on API requests only
+        'http_req_duration{pagoPaMethod:OtpCheckNoInvalidate}': ['p(95)<1000'], // threshold on API requests only
+        'http_req_duration{pagoPaMethod:OtpCheckInvalidate}': ['p(95)<1000'],
+        'http_req_duration{pagoPaMethod:CacheMiss}': ['p(95)<1000'],
+        'http_req_duration{pagoPaMethod:InvalidOtp}': ['p(95)<1000'],
+    },
+};
+
+function otpGeneration(fiscalCode, url, funcKey) {
+
+    var payload = JSON.stringify({});
+    var tag = {
+        pagoPaMethod: "OtpGeneration",
+    };
+    var headersParams = {
+        headers: {
+            'Content-Type': 'application/json',
+            'x-functions-key': funcKey
+        },
+    };
+    var r = http.post(url, payload, headersParams, {
+        tags: tag,
+    });
+    console.log("OTP generation for " + fiscalCode + ". Status " + r.status);
+    check(r, { 'status is 200': (r) => r.status === 200 }, tag);
+    return JSON.parse(r.body).code;
+}
+
+function merchantOtpCheck(otp, invalidate, urlBaseMerchantPath, funcMerchantKey, tag) {
+
+    var url = `${urlBaseMerchantPath}/api/v1/merchant/cgn/otp/validate`
+    var payload = JSON.stringify(
+        {
+            'otp_code': otp,
+            'invalidate_otp': Boolean(invalidate)
+        }
+    );
+    var headersParams = {
+        headers: {
+            'Content-Type': 'application/json',
+            'x-functions-key': funcMerchantKey
+        },
+    };
+
+    var r = http.post(url, payload, headersParams, {
+        tags: tag,
+    });
+    console.log(`Otp check invalidate ${invalidate}: ${otp} | Status ${r.status}`);
+    return r;
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export default function () {
+    var funcKey = `${__ENV.FUNC_KEY}`
+    var urlBasePath = `${__ENV.BASE_URL}`
+    var urlBaseMerchantPath = `${__ENV.BASE_MERCHANT_URL}`
+    var funcMerchantKey = `${__ENV.FUNC_MERCHANT_KEY}`
+    var otp = ""
+
+    // Known code for testing puroposes.
+    const fiscalCode = "HXHIKW90A29Y010X"
+    console.log('Fiscal code: ' + fiscalCode)
+
+
+    var random = getRandomInt(1, 100);
+    if (random == 100) {
+        // Simulating a cache miss on Redis for an expired token. (UGN0ILLIQH4) Every 100 tests (more or less...).
+        console.log(`Cache miss simulation.`)
+        var tag = {
+            pagoPaMethod: "CacheMiss",
+        };
+        otp = "9NA3RWTCVSS"
+        var r = merchantOtpCheck(otp, 0, urlBaseMerchantPath, funcMerchantKey);
+        check(r, { 'status is 404': (r) => r.status === 404 }, tag);
+    }
+    // Simulating an invalid OTP. Every 100 tests (more or less...).
+    else if (random == 99) {
+        // Simulating an invalid OTP.
+        console.log(`Invalid OTP simulation.`)
+        var tag = {
+            pagoPaMethod: "InvalidOtp",
+        };
+        otp = "invalid"
+        var r = merchantOtpCheck(otp, 0, urlBaseMerchantPath, funcMerchantKey);
+        check(r, { 'status is 400': (r) => r.status === 400 }, tag);
+    }
+    else {
+
+        // OTP generation.
+        var url = `${urlBasePath}/api/v1/cgn/otp/${fiscalCode}`;
+        var funcKey = `${__ENV.FUNC_KEY}`
+        var otp = otpGeneration(fiscalCode, url, funcKey)
+
+
+        // Simulate waiting for OTP usage by the user when inserting the promo code while doing a checkout.
+        sleep(getRandomInt(1, 3))
+
+        // Marchant calls API to check if OTP exists without invalidating the code itself.
+        var tag = {
+            pagoPaMethod: "OtpCheckNoInvalidate",
+        };
+        var r = merchantOtpCheck(otp, 0, urlBaseMerchantPath, funcMerchantKey);
+        check(r, { 'status is 200': (r) => r.status === 200 }, tag);
+
+        // Simulate waiting for OTP usage by the merchant when processing checkout.
+        sleep(getRandomInt(1, 3))
+
+        // Marchant calls API to consume the OTP.
+        r = merchantOtpCheck(otp, 1, urlBaseMerchantPath, funcMerchantKey);
+        check(r, { 'status is 200': (r) => r.status === 200 }, tag);
+
+
+    }
+
+    console.log('Test completed for: ' + fiscalCode + ' OTP: ' + otp);
+
+}
---
+++ src/user_activate_card_workflow.js
@@ -0,0 +1,125 @@
+//docker run -i --rm -v $(pwd)/src:/src  -e FUNC_KEY=${FUNC_KEY} -e BASE_URL=${BASE_URL} loadimpact/k6 run /src/user_activate_card_workflow.js
+
+import http from 'k6/http';
+import { sleep } from 'k6';
+import { check } from 'k6';
+import { generateFakeFiscalCode } from './modules/helpers.js';
+
+export let options = {
+    vus: 1,
+    stages: [
+        { duration: '30s', target: 1 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+    ],
+    thresholds: {
+        http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+        'http_req_duration{pagoPaMethod:ActivateNewCard}': ['p(95)<1000'], // threshold on API requests only
+        'http_req_duration{pagoPaMethod:GetCGNActivation}': ['p(95)<1000'], // threshold on API requests only
+        'http_req_duration{pagoPaMethod:GetCGNStatus}': ['p(95)<1000'], // threshold on API requests only
+        'http_req_duration{pagoPaMethod:GetEycaStatus}': ['p(95)<1000'], // threshold on API requests only
+    },
+};
+
+export default function () {
+    // Values from env var.
+    var urlBasePath = `${__ENV.BASE_URL}`
+    var funcKey = `${__ENV.FUNC_KEY}`
+
+    // Born in the ninenties.
+    var fiscalCode = generateFakeFiscalCode("9");
+    console.log('Fiscal code: ' + fiscalCode)
+    var headersParams = {
+        headers: {
+            'Content-Type': 'application/json',
+            'x-functions-key': funcKey,
+        },
+    };
+
+    // Activate new card.
+    var tag = {
+        pagoPaMethod: "ActivateNewCard",
+    };
+    var url = `${urlBasePath}/api/v1/cgn/${fiscalCode}/activation`;
+    var payload = JSON.stringify({});
+    var r = http.post(url, payload, headersParams, {
+        tags: tag,
+    });
+    console.log("Activate new card Fiscal code " + fiscalCode + " Status " + r.status);
+    check(r, { 'status is 201': (r) => r.status === 201 }, tag);
+
+    //Simulating mobile app polling to check activation status.
+    var maxRetries = 20;
+    var retryCount = 0;
+    var cardActivated = false;
+    do {
+        sleep(1)
+        console.log('Polling Get CGN Activation: ' + r.status + ' retry: ' + (retryCount + 1));
+
+        // Get CGN Activation (200 with one of the following statuses: completed, pending, running)
+        tag = {
+            pagoPaMethod: "GetCGNActivation",
+        };
+        url = `${urlBasePath}/api/v1/cgn/${fiscalCode}/activation`;
+        r = http.get(url, headersParams, {
+            tags: tag,
+        });
+        check(r, { 'Get CGN Activation is 200': (r) => (r.status === 200) },
+            tag
+        );
+
+        // If status is "completed" then the card is activated.
+        var jsonBody = JSON.parse(r.body);
+        if (jsonBody.status === 'COMPLETED') {
+            cardActivated = true;
+            break;
+        }
+        console.log('Get CGN Activation: ' + r.status);
+
+        retryCount++;
+    }
+    while ((retryCount < maxRetries) && !cardActivated);
+
+    if (cardActivated) {
+        console.log('Card ' + fiscalCode + ' activated.')
+    }
+    else {
+        console.log('Card ' + fiscalCode + ' NOT activated. Exit test.')
+        return;
+    }
+
+    // Simulating mobile app to get CGN and Eyca status.
+
+    // Get CGN status.
+    tag = {
+        pagoPaMethod: "GetCGNStatus",
+    };
+    url = `${urlBasePath}/api/v1/cgn/status/${fiscalCode}`;
+    r = http.get(url, headersParams, {
+        tags: tag,
+    });
+    console.log('Get CGN status: ' + r.status);
+    check(r, {
+        'Get CGN status 200': (r) => (r.status === 200)
+    },
+        tag
+    );
+
+    sleep(0.5);
+
+    //Get Eyca status.
+    tag = {
+        pagoPaMethod: "GetEycaStatus",
+    };
+    url = `${urlBasePath}/api/v1/cgn/eyca/status/${fiscalCode}`
+    var r = http.get(url, headersParams, {
+        tags: tag,
+    });
+    console.log('Get Eyca status: ' + r.status);
+    check(r, {
+        'Get Eyca status is 200': (r) => r.status === 200
+    }, tag);
+
+
+    console.log('Test completed for: ' + fiscalCode);
+
+
+}
