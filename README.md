5ba51ae7719727267531ee8d61a489c80480abca
Tommaso Stocchi <tommasostocchi@outlook.com>
2021-04-15 15:40:15 +0200

    target iteration per second in user_activate and generate_otp

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Load tests for CGN project
 
-This is a set of [k6](https://k6.io) load tests related to the CGN (carta giovani nazionale) initiative
+This is a set of [k6](https://k6.io) load tests related to the CGN (carta giovani nazionale) initiative.
+
+The [new card activation](#01.-Function-Info-page) and [OTP generation](#06.-Generate-OTP) tests require a set of parameters: **rate**, **duration**, **preAllocatedVUs** and **maxVUs**. These parameters are necessary to set the test target to a given number of iteration per second (**rate**) in a given time (**duration**), using a certain number of VUs (**preAllocatedVUs** and **maxVUs**).
 
 
 ## 01. Function Info page
--- src/generate_otp.js
+++ src/generate_otp.js
@@ -3,9 +3,15 @@ import { check } from 'k6';
 import { generateFakeFiscalCode } from './modules/helpers.js';
 
 export let options = {
-  stages: [
-    { duration: '10s', target: 1 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
-  ],
+  scenarios: {
+    contacts: {
+        executor: 'constant-arrival-rate',
+        rate: __ENV.rate, // e.g. 20000 for 20K iterations
+        duration: __ENV.duration, // e.g. '1m'
+        preAllocatedVUs: __ENV.preAllocatedVUs, // e.g. 500
+        maxVUs: __ENV.maxVUs // e.g. 1000
+    }
+  },
   thresholds: {
     http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
     'logged in successfully': ['p(99)<1500'], // 99% of requests must complete below 1.5s
